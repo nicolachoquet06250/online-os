90b989f629f1884e74331eaad9b6a34570fc697b
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-05-13 14:05:46 +0200

    create window extensible system

--- routes/settings.js
+++ routes/settings.js
@@ -5,4 +5,7 @@ let router = express.Router();
 router.get('/', (req, res, next) => res.render('window_settings', {}));
 
 router.get('/me', (req, res, next) => res.render('window_settings_account', {}));
+
+router.get('/start_menu', (req, res, next) => res.render('window_settings_start_menu', {}));
+
 module.exports = router;
